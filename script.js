5a0356951a5bca6d47e3b3c42046315db5118fcd
Mikhail <53335687+mishapolyakov@users.noreply.github.com>
2022-01-26 21:47:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 
 function Calc ( a, b, operator ) {
 
-    if( typeof a != 'number' || typeof b != 'number') {
+    if( typeof a != 'number' || typeof b != 'number' ) {
         return 'Error'
     } 
 
-    if ( operator === 'sum') {
+    if ( operator === 'sum' ) {
         return a + b
-    } else if ( operator === 'dif') {
+    } else if ( operator === 'dif' ) {
         return a - b
-    } else if (operator === 'multi') {
+    } else if (operator === 'multi' ) {
         return a * b
-    } else if ( operator === 'div') {
+    } else if ( operator === 'div' ) {
         return a / b
     }
 }
